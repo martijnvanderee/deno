996998976e4dbbf20146d476511fcda7b392ba50
“broodbakker” <mrtjnvanderee@gmail.com>
2025-04-01 21:49:46 +0200

    test

--- main.ts
+++ main.ts
@@ -1,25 +1,31 @@
-import 'jsr:@std/dotenv/load';
-import { serveFile } from 'jsr:@std/http/file-server';
+import { Application, Router } from "@oak/oak";
+import { oakCors } from "@tajpouria/cors";
+import data from "./data.json" with { type: "json" };
 
-const PORT = Deno.env.get('PORT') || 8000;
+const router = new Router();
 
-const handler = async (req: Request): Promise<Response> => {
-  const url = new URL(req.url);
+router
+    .get("/", (context) => {
+        context.response.body = "Welcome to dinosaur API!";
+    })
+    .get("/dinosaurs", (context) => {
+        context.response.body = data;
+    })
+    .get("/dinosaurs/:dinosaur", (context) => {
+        if (!context?.params?.dinosaur) {
+            context.response.body = "No dinosaur name provided.";
+        }
 
-  if (url.pathname === '/') {
-    const greeting = Deno.env.get('GREETING') || 'Hello from Deno 1!';
-    return new Response(greeting);
-  } else if (url.pathname === '/greet') {
-    const greeting = Deno.env.get('GREETING') || 'Hello from Deno 2!';
-    return new Response(greeting);
-  } else {
-    return new Response('Not Found', { status: 404 });
-  }
-};
+        const dinosaur = data.find((item) =>
+            item.name.toLowerCase() === context.params.dinosaur.toLowerCase()
+        );
 
-// Error handling
-try {
-  Deno.serve({ port: Number(PORT) }, handler);
-} catch (err) {
-  console.error('Error starting the server:', err);
-}
+        context.response.body = dinosaur ? dinosaur : "No dinosaur found.";
+    });
+
+const app = new Application();
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
